7ea7a87e6038a1c45a2dbb670745f7e3494e03bd
Lex-101 <2425401590@live.gctu.edu.gh>
2026-05-31 21:55:40 +0000

    Completed finial assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,39 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Alex Kwadwo Sasu Dartey
+# STUDENT ID: 2425401590
+# Email: 2425401590@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Alex Kwadwo Sasu Dartey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
   pass #code goes here
 
+# Prints my initials in large block letters.
+
+  print()
+
+  print("...AKSD")
+
+  print()
+
+  print("...AAAAAAAAAAAA........KK........KK..........SSSSSSSSSS........DDDDDDDDD........")
+  print("...AA........AA........KK......KK...........SSSSSSSSSSS........DD........DD.....")
+  print("...AA........AA........KK....KK............SSSS................DD........DD.....")
+  print("...AA........AA........KK..KK..............SSSS................DD........DD.....")
+  print("...AAAAAAAAAAAA........KKKK..................SSSSSS............DD........DD.....")
+  print("...AA........AA........KK..KK..................SSSSSS..........DD........DD.....")
+  print("...AA........AA........KK....KK....................SSSS........DD........DD.....")
+  print("...AA........AA........KK......KK..................SSSS........DD........DD.....")
+  print("...AA........AA...**...KK........KK...**...SSSSSSSSSSS....**...DD........DD...**")
+  print("...AA........AA...**...KK........KK...**...SSSSSSSSSS.....**...DDDDDDDDD......**")
+
+  print()
 
 
 main()
